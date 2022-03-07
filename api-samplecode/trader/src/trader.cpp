ca979bb175db582ab44ecfe41c065128ea76f7da
Bhautik Gosai <bhautikng143@gmail.com>
2022-03-07 13:05:26 +0530

    replace order

--- api-samplecode/trader/src/trader.cpp
+++ api-samplecode/trader/src/trader.cpp
@@ -564,10 +564,10 @@ int main() {
     doSleep(5000);
     // tradeClient->insertOrder("GC2204-CME", false, 4000.0, 2);
     // doSleep(1000);
-    tradeClient->queryOrder("GC2204-CME");
-    doSleep(1000);
-    tradeClient->replaceOrder("1", 1986, 2);
-    doSleep(1000);
+    // tradeClient->queryOrder("GC2204-CME");
+    // doSleep(1000);
+    // tradeClient->replaceOrder("1", 1986, 2);
+    // doSleep(1000);
     // tradeClient->cancelOrder("1");
     // doSleep(1000);
     tradeClient->queryTrade("GC2204-CME");
